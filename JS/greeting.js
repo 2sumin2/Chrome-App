1e9ec9fcb57ba10e6ad5281aaa79f155d5dc158b
sumin <lsm001005@naver.com>
2022-02-21 23:44:02 +0900

    Fix the delete-all button position

--- JS/greeting.js
+++ JS/greeting.js
@@ -3,12 +3,14 @@ const loginInput = loginForm.querySelector("input");
 const sayHello = document.querySelector("#greeting");
 const savedUsername = localStorage.getItem("username");
 const taskTable = document.querySelector(".weeklytask");
+const clearBtn = document.querySelector("#deleteAll");
 
 function onLogIn(event) {
     event.preventDefault();
     const username = loginInput.value;
     loginForm.classList.add("hidden");
     taskTable.classList.remove("hidden");
+    clearBtn.classList.remove("hidden");
     localStorage.setItem("username", username);
     printGreeting(username);
 }
@@ -23,6 +25,7 @@ if (savedUsername == null) {
 } else {
     printGreeting(savedUsername);
     taskTable.classList.remove("hidden");
+    clearBtn.classList.remove("hidden");
 }
 
 loginForm.addEventListener("submit", onLogIn);
