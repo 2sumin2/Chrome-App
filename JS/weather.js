a52db47b13bf9a4bff05a9db07695972f315a592
sumin <lsm001005@naver.com>
2022-02-22 00:46:25 +0900

    Add weather icon

--- JS/weather.js
+++ JS/weather.js
@@ -4,13 +4,17 @@ function onGeoOk(position) {
     const lat = position.coords.latitude;
     const lon = position.coords.longitude;
     const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
-
+    
     fetch(url)
         .then((response) => response.json())
         .then((data) => {
         const weatherConainer = document.querySelector("#weather span");
-        weatherConainer.innerText = `${data.name}  ${data.weather[0].main} / ${data.main.temp}°`; 
+        const iconId = data.weather[0].icon;
+        const iconURL = "http://openweathermap.org/img/w/" + iconId + ".png";
+        document.querySelector("#weatherIcon").src = iconURL;
+        weatherConainer.innerText = `${data.name} / ${data.main.temp}°`; 
         });
+    
 }
 function onGeoError() {
     alert("Can't find you. No weather for you.");
