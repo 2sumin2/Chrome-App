af7a7ab9d24e3163ba9806598be45ba654d4013f
sumin <lsm001005@naver.com>
2022-03-09 19:47:42 +0900

    Correct errors when the winodw is resized

--- JS/greeting.js
+++ JS/greeting.js
@@ -6,10 +6,24 @@ const taskTable = document.querySelector(".weeklytask");
 const clearBtn = document.querySelector("#deleteAll");
 const sideBox = document.querySelector(".sidebox");
 const mainBox = document.querySelector(".mainbox");
+const ths = taskTable.querySelectorAll("th");
+const tds = taskTable.querySelectorAll("td");
+
+function resizehandlerMore() {
+    for(i=0; i<3;i+=2){
+        ths[i].classList.remove("hidden");
+    }
+    for (i = 0; i < 7; i++) {
+        tds[i].classList.remove("hidden");
+        ths[i+3].classList.remove("hidden");
+        tds[i].classList.remove("zerosize");
+        ths[i+3].classList.remove("zerosize");
+        tds[i].classList.remove("fullsize");
+        ths[i+3].classList.remove("fullsize");
+        }
+}
 
 function resizehandler() {
-    const ths = taskTable.querySelectorAll("th");
-    const tds = taskTable.querySelectorAll("td");
     if (window.outerWidth < 600 ) {
         sideBox.classList.add("hidden");
         mainBox.style.width = "95%";
@@ -32,22 +46,11 @@ function resizehandler() {
         //taskTable.style = "margin-left:0";
         sideBox.classList.add("hidden");
         mainBox.style.width = "95%";
-        for(i=0; i<3;i+=2){
-            ths[i].classList.remove("hidden");
-        }
-        for (i = 0; i < 7; i++) {
-            tds[i].classList.remove("hidden");
-            ths[i+3].classList.remove("hidden");
-            tds[i].classList.remove("zerosize");
-            ths[i+3].classList.remove("zerosize");
-            tds[i].classList.remove("fullsize");
-            ths[i+3].classList.remove("fullsize");
-            }
-        
-
+        resizehandlerMore();
     } else {
         sideBox.classList.remove("hidden");
         mainBox.style.width = "80%";
+        resizehandlerMore();
     }
 }
 
