48d9295f2162d36e9d54d0a7b134e785f6297b84
sumin <lsm001005@naver.com>
2022-03-27 20:17:38 +0900

    Change the structure of storing in localstorage

--- JS/monthlytodo.js
+++ JS/monthlytodo.js
@@ -7,10 +7,7 @@ const nextMonBtn = monthlyTaskTable.querySelector("#nextmonth");
 //let month = today.getMonth();
 //let date = today.getDate();
 
-let monthlyToDos = [];
-for (i=0;i<32;i++){
-    monthlyToDos[i] = [];
-}
+let monthlyToDos = [[]];
 
 printCalender(year, month, date);
 let monthlyToDoList = document.querySelectorAll(".monthly-todo-list");
@@ -19,25 +16,23 @@ let monthlyToDoInput = document.querySelectorAll('.monthly-newtodo');
 
 
 
-function saveMonthlyTask (day) {
-    day = parseInt(day);
-    localStorage.setItem(day+6, JSON.stringify(monthlyToDos[day]));
+function saveMonthlyTask(day) {
+    localStorage.setItem(day, JSON.stringify(monthlyToDos[0][day]));
 }
 
-function deleteMonthlyTask (event) {
+function deleteMonthlyTask(event, day) {
     const li = event.target.parentElement;
-    const day = li.parentElement.id;
-    monthlyToDos[day] = monthlyToDos[day].filter((toDo) => toDo.id !== parseInt(li.id));
+    monthlyToDos[0][day] = monthlyToDos[0][day].filter((toDo) => toDo.id !== parseInt(li.id));
     li.remove();
     saveMonthlyTask(day);
 }
 
-function finishMonthlyTask(event) {
+function finishMonthlyTask(event, day) {
     const li = event.target.parentElement;
-    const day = li.parentElement.id;
-    for (i=0; i<monthlyToDos[day].length;i++) {
-        if (monthlyToDos[day][i].id == li.id){
-            const changeElement = monthlyToDos[day][i];
+    console.log(monthlyToDos);
+    for (i = 0; i < monthlyToDos[0][day].length; i++) {
+        if (monthlyToDos[0][day][i].id == li.id) {
+            const changeElement = monthlyToDos[0][day][i];
             if (changeElement.done == "yes") {
                 event.target.innerText = "☐ ";
                 changeElement.done = "no";
@@ -52,6 +47,8 @@ function finishMonthlyTask(event) {
 }
 
 function printMonthlyTask(day, newToDo) {
+    const toDoDay = new Date(day);
+    const toDoDate = toDoDay.getDate();
     monthlyToDoList = document.querySelectorAll(".monthly-todo-list");
     const li = document.createElement("li");
     li.id = newToDo.id;
@@ -60,46 +57,67 @@ function printMonthlyTask(day, newToDo) {
     const span = document.createElement("span");
     const deleteBtn = document.createElement("button");
     span.innerText = newToDo.text;
+    console.log(newToDo.text);
     if (newToDo.done == "yes") {
         checkBtn.innerText = "✓ ";
         li.classList.add("done");
     } else {
-        checkBtn.innerText =  "☐ ";
+        checkBtn.innerText = "☐ ";
     }
     checkBtn.classList.toggle("check");
     deleteBtn.innerText = " 〤";
-    deleteBtn.addEventListener("click", deleteMonthlyTask);
-    checkBtn.addEventListener("click", finishMonthlyTask);
+    deleteBtn.addEventListener("click", () => {
+        deleteMonthlyTask(event, day);
+    });
+    checkBtn.addEventListener("click", () => {
+        finishMonthlyTask(event, day);
+    });
     li.appendChild(checkBtn);
     li.appendChild(span);
     li.appendChild(deleteBtn);
-    monthlyToDoList[day-1].appendChild(li);
+    monthlyToDoList[toDoDate - 1].appendChild(li);
 }
 function MonthlyTaskSubmit(day, event) {
+    const toDoDay = new Date(day);
+    const toDoDate = toDoDay.getDate();
     event.preventDefault();
     monthlyToDoForm = document.querySelectorAll(".monthly-todo-list-form");
     monthlyToDoInput = document.querySelectorAll('.monthly-newtodo');
-    const newToDo = monthlyToDoInput[day-1].value;
-    if(newToDo.split() != "") {
+    const newToDo = monthlyToDoInput[toDoDate - 1].value;
+    if (newToDo.split() != "") {
         const newToDoObj = {
-            text:newToDo,
+            text: newToDo,
             id: Date.now(),
-            done : "no"
+            done: "no"
         }
-    monthlyToDos[day].push(newToDoObj);
-    monthlyToDoInput[day-1].value = "";
-    printMonthlyTask(day, newToDoObj);
-    saveMonthlyTask(day);
+        if (!monthlyToDos[0][day]) {
+            monthlyToDos[0][day] = [];
+        }
+        monthlyToDos[0][day].push(newToDoObj);
+
+        monthlyToDoInput[toDoDate - 1].value = "";
+        printMonthlyTask(day, newToDoObj);
+        saveMonthlyTask(day);
     };
 }
 
 function getLocalStorage() {
-    for (i=0; i<32; i++) {
-        const savedmonthlyToDos = localStorage.getItem(i+7);
-        if(savedmonthlyToDos != null){
-            const parsemonthlyToDos = JSON.parse(savedmonthlyToDos);  
-            monthlyToDos[i] = parsemonthlyToDos;
-            parsemonthlyToDos.forEach((item)=> (printMonthlyTask(i+1, item), monthlyToDos[i+1].push(item)));
+    for (i = 1; i <= 31; i++) {
+        if (localStorage.getItem(`${year}-${month + 1}-${i}`)) {
+            const savedmonthlyToDos = localStorage.getItem(`${year}-${month + 1}-${i}`);
+            if (savedmonthlyToDos != null && JSON.parse(savedmonthlyToDos).length != 0) {
+                let parsemonthlyToDos = JSON.parse(savedmonthlyToDos);
+                parsemonthlyToDos.forEach(
+                    (item) => (
+                        printMonthlyTask(`${year}-${month + 1}-${i}`, item),
+                        (
+                            !monthlyToDos[0][`${year}-${month + 1}-${i}`] ?
+                                monthlyToDos[0][`${year}-${month + 1}-${i}`] = [] : null
+                        ),
+                        monthlyToDos[0][`${year}-${month + 1}-${i}`].push(item),
+                    )
+                )
+            }
         }
     }
 }
@@ -107,41 +125,58 @@ function getLocalStorage() {
 function printDate(date, firstDay) {
     cell = row.insertCell();
     cell.id = i;
-    cell.innerHTML = i + 
+    cell.innerHTML = i +
         `<ul class="monthly-todo-list" id="${i}"></ul>
-        <form class="monthly-todo-list-form" id="${i}" onsubmit="MonthlyTaskSubmit(this.id, event)">
-        <input class="monthly-todo-input" id="${year}-${month}-${i}" type="submit" value="+" style="background-color:transparent; cursor:pointer; border:0; font-size: 13pt;"/>
-        <b><input class="monthly-newtodo" maxlength="30" type="text" style="font-size:11pt" placeholder="write.."/></b>
+        <form
+            class="monthly-todo-list-form"
+            id="${year}-${month + 1}-${i}"
+            onsubmit="MonthlyTaskSubmit(this.id, event)"
+        >
+            <input
+                class="monthly-todo-input"
+                id="${year}-${month + 1}-${i}"
+                type="submit" 
+                value="+" 
+                style="background-color:transparent; cursor:pointer; border:0; font-size: 13pt;"
+            />
+            <b>
+            <input 
+                class="monthly-newtodo" 
+                maxlength="30" 
+                type="text" 
+                style="font-size:11pt" placeholder="write.."
+            />
+            </b>
         </form></td>`;
 
-    if(firstDay==7){
+    if (firstDay == 7) {
         cell.style = "color:brown";
-    } else if(firstDay == 6){
+    } else if (firstDay == 6) {
 
         cell.style = "color:rgb(47, 47, 167)";
     }
-    if(i==date){
+    if (i == date) {
         const style = cell.getAttribute("style") + ";";
-        cell.setAttribute("style", style + "background-color:rgba(255,228,196, 0.2);" );
+        cell.setAttribute("style", style + "background-color:rgba(255,228,196, 0.2);");
     }
 }
 
-function printCalender (year, month, date) {
-    let lastDate = new Date(year,month+1,0).getDate();
-    let firstDay = new Date(year,month,1).getDay();
-    yearMonth.innerText = `${year} - ${String(month+1).padStart(2, "0")}`;
-	row = monthlyTaskTable.insertRow();
-	for(i=0; i<firstDay; i++){
-		cell = row.insertCell();
-        if(i==0){
+function printCalender(year, month, date) {
+    let lastDate = new Date(year, month + 1, 0).getDate();
+    let firstDay = new Date(year, month, 1).getDay();
+    yearMonth.innerText = `${year} - ${String(month + 1).padStart(2, "0")}`;
+    row = monthlyTaskTable.insertRow();
+    for (i = 0; i < firstDay; i++) {
+        cell = row.insertCell();
+        if (i == 0) {
             cell.style = "color:brown";
         }
     }
-    for(i=1; i<=lastDate; i++){
-        if(firstDay != 7){
+    for (i = 1; i <= lastDate; i++) {
+        if (firstDay != 7) {
             printDate(date, firstDay);
             firstDay += 1;
-        } else{
+        } else {
             row = monthlyTaskTable.insertRow();
             printDate(date, firstDay);
             firstDay = firstDay - 6;
@@ -151,17 +186,17 @@ function printCalender (year, month, date) {
 
 function resetCalendar() {
     while (monthlyTaskTable.rows.length > 2) {
-        monthlyTaskTable.deleteRow(monthlyTaskTable.rows.length-1);
+        monthlyTaskTable.deleteRow(monthlyTaskTable.rows.length - 1);
     }
 }
 
 function previousMonth() {
     month -= 1;
     resetCalendar();
-    if(month == -1){
-		year -= 1;
-		month = 12;
-	}
+    if (month == -1) {
+        year -= 1;
+        month = 12;
+    }
     printCalender(year, month, date);
     getLocalStorage();
 }
@@ -169,10 +204,10 @@ function previousMonth() {
 function nextMonth() {
     month += 1;
     resetCalendar();
-    if(month == 13){
-		year += 1;
-		month = 1;
-	}
+    if (month == 13) {
+        year += 1;
+        month = 1;
+    }
     printCalender(year, month, date);
     getLocalStorage();
 }
--- JS/weeklytodo.js
+++ JS/weeklytodo.js
@@ -6,12 +6,13 @@ const categoryMonth = document.querySelector("#category1");
 const categoryWeek = document.querySelector("#category2");
 const yearMonthWeek = document.querySelector("#year-month-week");
 
-const toDos = [[],[],[],[],[],[],[]];
-function saveToDo (day) {
+const toDos = [[], [], [], [], [], [], []];
+//[task, ["0":[]]]
+function saveToDo(day) {
     localStorage.setItem(day, JSON.stringify(toDos[day]));
 }
 
-function deleteToDo (event) {
+function deleteToDo(event) {
     const li = event.target.parentElement;
     const day = li.parentElement.id;
     toDos[day] = toDos[day].filter((toDo) => toDo.id !== parseInt(li.id));
@@ -23,9 +24,9 @@ function deleteToDo (event) {
 function finishToDo(event) {
     const li = event.target.parentElement;
     const day = li.parentElement.id;
-    
-    for (i=0; i<toDos[day].length;i++) {
-        if (toDos[day][i].id == li.id){
+
+    for (i = 0; i < toDos[day].length; i++) {
+        if (toDos[day][i].id == li.id) {
             const changeElement = toDos[day][i];
             if (changeElement.done == "yes") {
                 event.target.innerText = "☐ ";
@@ -52,7 +53,7 @@ function printToDo(day, newToDo) {
         checkBtn.innerText = "✓ ";
         li.classList.add("done");
     } else {
-        checkBtn.innerText =  "☐ ";
+        checkBtn.innerText = "☐ ";
     }
     checkBtn.classList.toggle("check");
     deleteBtn.innerText = " 〤";
@@ -67,28 +68,28 @@ function printToDo(day, newToDo) {
 function toDoSubmit(day, event) {
     event.preventDefault();
     console.log(day);
-    
+
     const newToDo = toDoInput[day].value;
-    if(newToDo.split() != "") {
+    if (newToDo.split() != "") {
         const newToDoObj = {
-            text:newToDo,
+            text: newToDo,
             id: Date.now(),
-            done : "no"
+            done: "no"
         }
         toDos[day].push(newToDoObj);
         toDoInput[day].value = "";
         printToDo(day, newToDoObj);
         saveToDo(day);
     };
-    
+
 }
 
-for (i=0; i<7; i++) {
+for (i = 0; i < 7; i++) {
     const savedToDos = localStorage.getItem(i);
-    if(savedToDos != null){
-        const parseToDos = JSON.parse(savedToDos);  
+    if (savedToDos != null) {
+        const parseToDos = JSON.parse(savedToDos);
         toDos[i] = parseToDos;
-        parseToDos.forEach((item)=> printToDo(i, item));
+        parseToDos.forEach((item) => printToDo(i, item));
     }
 }
 
@@ -96,19 +97,19 @@ const today = new Date();
 let year = today.getFullYear();
 let month = today.getMonth();
 let date = today.getDate();
-const firstDateDay = new Date(year,month,1).getDay();
+const firstDateDay = new Date(year, month, 1).getDay();
 const thisWeek = Math.ceil((firstDateDay + date) / 7);
 const week = ["1st", "2nd", "3rd", "4th", "5th"]
 
-yearMonthWeek.innerText = `${year} - ${monthNames[month]} - ${week[thisWeek-1]} week`;
+yearMonthWeek.innerText = `${year} - ${monthNames[month]} - ${week[thisWeek - 1]} week`;
 
 function clearTable() {
-    if(taskTable.classList.contains("hidden")) {
-        for (i=7; i<38; i++) {
+    if (taskTable.classList.contains("hidden")) {
+        for (i = 7; i < 38; i++) {
             localStorage.removeItem(i);
         }
-    } else{
-        for (i=0; i<7; i++) {
+    } else {
+        for (i = 0; i < 7; i++) {
             localStorage.removeItem(i);
         }
     }
@@ -117,11 +118,11 @@ function clearTable() {
 
 deleteAllBtn.addEventListener("click", clearTable);
 
-categoryMonth.addEventListener("click", function() {
+categoryMonth.addEventListener("click", function () {
     taskTable.classList.add("hidden");
     monthlyTaskTable.classList.remove("hidden");
 });
-categoryWeek.addEventListener("click", function() {
+categoryWeek.addEventListener("click", function () {
     taskTable.classList.remove("hidden");
     monthlyTaskTable.classList.add("hidden");
 });
